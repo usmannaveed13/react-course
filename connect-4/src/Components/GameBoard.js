56061641cb61f3f7d731573d4f09bed4313a714b
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-11-22 12:56:49 +0500

    now adding new function for taking circle repeatable part into for loop and here is intersting thing is that its render till we take retrun here, because any function is not render as JSX till we take return

--- connect-4/src/Components/GameBoard.js
+++ connect-4/src/Components/GameBoard.js
@@ -2,24 +2,40 @@ import React, {useState} from "react";
 import GameCircle from "./GameCircle";
 import '../Game.css';
 
+const NO_CIRCLE = 16;
 const NO_PLAYER = 0;
 const PLAYER_1 = 1;
 const PLAYER_2 = 2;
 
 
 const GameBoard = () => {
-   const [gameBoard, setGameBoard] = useState(Array(16).fill(0));
+   const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
    const [currentPalyer, setCurrentPalyer] = useState(PLAYER_1);
 
    console.log(gameBoard);
+
+   const initBoard = () => {
+        const cicles = [];
+
+        for (let i = 0; i < NO_CIRCLE; i++) {
+            cicles.push(renderCircle(i));
+        }
+        return cicles;
+   } 
+
 
    const circleClicked = (id) => {
     
           console.log('circle clicked:' + id);
 
-          const board = [...gameBoard];
-          board[id] = currentPalyer;
-          setGameBoard(board);
+          setGameBoard(prev => {
+            return prev.map((circle, pos) => {
+                 if (pos === id) return currentPalyer;
+                 return circle; 
+            })
+          })
+
+
 
           setCurrentPalyer(currentPalyer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
 
@@ -33,15 +49,7 @@ const GameBoard = () => {
 
     return (
         <div className="gameBoard">
-            {renderCircle(0)}
-            {renderCircle(1)}
-            {renderCircle(2)}
-            {renderCircle(3)}
-            {renderCircle(4)}
-            {renderCircle(5)}
-            {renderCircle(6)}
-            {renderCircle(7)}
-            {renderCircle(8)}
+          {initBoard()}
         </div>
     )
 }
